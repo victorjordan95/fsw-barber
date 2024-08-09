3e32788aaf4e4994a5f94aa290381d3ff2082a0f
Victor Jordan <victor.jordan@mercadolivre.com>
2024-08-09 09:04:57 -0300

    feat: handling login with google

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,12 @@
-import { SearchIcon } from "lucide-react"
-import Image from "next/image"
-import BarbershopItem from "./_components/barbershop-item"
-import BookingItem from "./_components/booking-item"
 import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
-import { Card, CardContent } from "./_components/ui/card"
-import { Input } from "./_components/ui/input"
-import { quickSearchOptions } from "./_constants/search"
+import Image from "next/image"
 import { db } from "./_lib/prisma"
-import Footer from "./_components/footer"
+import BarbershopItem from "./_components/barbershop-item"
+import { quickSearchOptions } from "./_constants/search"
+import BookingItem from "./_components/booking-item"
+import Search from "./_components/search"
+import Link from "next/link"
 
 const Home = async () => {
   const barbershops = await db.barbershop.findMany({})
@@ -28,24 +26,28 @@ const Home = async () => {
         <p>Segunda-feira, 05 de agosto.</p>
 
         {/* BUSCA */}
-        <div className="mt-6 flex items-center gap-2">
-          <Input placeholder="Faça sua busca..." />
-          <Button>
-            <SearchIcon />
-          </Button>
+        <div className="mt-6">
+          <Search />
         </div>
 
         {/* BUSCA RÁPIDA */}
         <div className="mt-6 flex gap-3 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
           {quickSearchOptions.map((option) => (
-            <Button className="gap-2" variant="secondary" key={option.title}>
-              <Image
-                src={option.imageUrl}
-                width={16}
-                height={16}
-                alt={option.title}
-              />
-              {option.title}
+            <Button
+              className="gap-2"
+              variant="secondary"
+              key={option.title}
+              asChild
+            >
+              <Link href={`/barbershops?service=${option.title}`}>
+                <Image
+                  src={option.imageUrl}
+                  width={16}
+                  height={16}
+                  alt={option.title}
+                />
+                {option.title}
+              </Link>
             </Button>
           ))}
         </div>
